f28a23fa6c0159a7dafaa5c2bc56989704e132de
박희령 <129574248+heerpark@users.noreply.github.com>
2024-10-01 10:37:12 +0900

    [Gold V] Title: 배, Time: 100 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -2,40 +2,25 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-categorized = [0] * n
 crain = list(map(int, input().split()))
-crain.sort()
 m = int(input())
 box = list(map(int, input().split()))
+
+crain.sort(reverse=True)
+box.sort(reverse=True)
 time = 0
 
-flag = False
-for i in box:
-    if i > crain[-1]:
-        flag = True
-
-for i in box:
-    for j in range(len(crain)):
-        if i <= crain[j]:
-            categorized[j] += 1
-            break
-
-while sum(categorized) > 0:
-    # print(categorized)
-    for i in range(len(categorized)):
-        if categorized[i] > 0:
-            categorized[i] -= 1
-        else:
-            # print("hi")
-            for j in range(i-1, -1, -1):
-                # print("j:", j)
-                if categorized[j] > 0:
-                    categorized[j] -= 1
-                    break
-    # print("------------------")
-    time += 1
-
-if flag:
-    print(-1)
+if crain[0] < box[0]:
+    time = -1
 else:
-    print(time)
+    while box:
+        for i in crain:
+            if box and i < box[-1]:
+                continue
+            for j in box:
+                if i >= j:
+                    box.remove(j)
+                    break
+        time += 1
+
+print(time)
