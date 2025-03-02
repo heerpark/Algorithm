4c424c5e9ca4bb2eefb7888b395d97eb2007e8f4
박희령 <129574248+heerpark@users.noreply.github.com>
2025-03-03 04:36:30 +0900

    [Gold IV] Title: 인구 이동, Time: 1072 ms, Memory: 128524 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,7 +1,6 @@
 import sys
-
+from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 n, l, r = map(int, input().split())
 data = []
@@ -11,48 +10,44 @@ dcol = [0, 0, 1, -1]
 for _ in range(n):
     data.append(list(map(int, input().split())))
 
+def bfs(row, col, group):
+    q = deque()
+    result = []
+    q.append((row, col))
+    while q:
+        now = q.popleft()
+        for i in range(4):
+            nrow = now[0] + drow[i]
+            ncol = now[1] + dcol[i]
+            if 0 <= nrow < n and 0 <= ncol < n and group_table[nrow][ncol] == 0:
+                if l <= abs(data[now[0]][now[1]] - data[nrow][ncol]) <= r:
+                    q.append((nrow, ncol))
+                    group_table[nrow][ncol] = group
+                    result.append((nrow, ncol))
+    return result
 
-def merge(group):
-    for i in range(1, group):
-        total = 0
-        position = []
-        for row in range(n):
-            for col in range(n):
-                if group_table[row][col] == i:
-                    total += data[row][col]
-                    position.append((row, col))
-        temp = total // len(position)
-        for row, col in position:
-            data[row][col] = temp
-
-
-
-def dfs(i, j, group):
-    flag = False
-    for k in range(4):
-        nr = i + drow[k]
-        nc = j + dcol[k]
-        if 0 <= nr < n and 0 <= nc < n and group_table[nr][nc] == 0:
-            if l <= abs(data[i][j] - data[nr][nc]) <= r:
-                group_table[i][j] = group
-                group_table[nr][nc] = group
-                dfs(nr, nc, group)
-                flag = True
-    return flag
-
+def merge(info):
+    total = 0
+    for row, col in info:
+        total += data[row][col]
+    avg = total // len(info)
+    for row, col in info:
+        data[row][col] = avg
 
 day = 0
+
 while True:
     group = 1
     group_table = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            if dfs(i, j, group) == True:
-                group += 1
-
+            if group_table[i][j] == 0:
+                result = bfs(i, j, group)
+                if len(result) > 0:
+                    merge(result)
+                    group += 1
     if group == 1:
         print(day)
         break
-    #국경이동계산
-    merge(group)
     day += 1
+
